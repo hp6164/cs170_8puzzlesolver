c60512eb926fd2282ce4b29b643d2e140675ccc9
edgar2020 <edgar2020@chromium.org>
2023-04-22 23:38:16 -0700

    managed to make UCS aka just a bfs to work

--- src/node.cpp
+++ src/node.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Node::Node(vector<vector<int>> initTable, Node* p)//use this to create a child node
+Node::Node(vector<vector<int>> initTable, Node* p, validMoves last)//use this to create a child node
 {
     try
     {
@@ -11,6 +11,8 @@ Node::Node(vector<vector<int>> initTable, Node* p)//use this to create a child n
         downMove = nullptr;
         rightMove = nullptr;
         downMove = nullptr;
+
+        lastMove = last;
 
         board = initTable;
         for(int i = 0; i < initTable.size(); i++)
@@ -46,6 +48,8 @@ Node::Node(vector<vector<int>> initTable)// use this to make root node
         rightMove = nullptr;
         downMove = nullptr;
 
+        lastMove = START;
+
         board = initTable;
         for(int i = 0; i < initTable.size(); i++)
         {
@@ -78,6 +82,8 @@ Node::Node()//default input
     rightMove = nullptr;
     downMove = nullptr;
 
+    lastMove = START;
+
     board = { {1, 0, 3}, 
               {4, 2, 6},
               {7, 5, 8} };
@@ -85,17 +91,25 @@ Node::Node()//default input
     zero_row = 0;
 }
 
-Node::Node(Node &temp)//#TODO come back to this later
+Node::Node(Node* temp)//Will make a copy of the board and nothing else
 {
-    parent = temp.parent;
-    board = temp.board;
-    zero_col = temp.zero_col;
-    zero_row = temp.zero_row;
+    parent = nullptr;
+    upMove = nullptr;
+    downMove = nullptr;
+    rightMove = nullptr;
+    downMove = nullptr;
+    board = temp->board;
+    zero_col = temp->zero_col;
+    zero_row = temp->zero_row;
 }//end of copy Consturctor
 
 Node::~Node()
 {
     delete parent;
+    delete upMove;
+    delete downMove;
+    delete rightMove;
+    delete downMove;
 }
 
 
@@ -192,3 +206,174 @@ void Node::printNode()
         }//end of outer loop
 }//end of Print Node
 
+
+Node* Node::Up()
+{
+    try
+    {
+        if(zero_row != 0  && lastMove != DOWN)
+        {
+            upMove = new Node(this);
+            upMove->board.at(zero_row).at(zero_col) = upMove->board.at(zero_row-1).at(zero_col);
+            upMove->board.at(zero_row-1).at(zero_col) = 0; 
+
+            upMove->zero_row--;
+
+            upMove->parent = this;
+            upMove->lastMove = UP;
+            
+            return upMove;
+        }
+        else
+        {
+            throw invalid_argument("Error in tryUp");
+        }
+    }//end of try
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return nullptr;
+    }//end of catch
+}//end of up
+
+Node* Node::Down()
+{
+    try
+    {
+        if(zero_row != board.size()-1 && lastMove != UP)
+        {
+            downMove = new Node(this);
+            downMove->board.at(zero_row).at(zero_col) = downMove->board.at(zero_row+1).at(zero_col);
+            downMove->board.at(zero_row+1).at(zero_col) = 0; 
+
+            downMove->zero_row++;
+
+            downMove->parent = this;
+            downMove->lastMove = DOWN;
+            return downMove;
+        }
+        else
+        {
+            throw invalid_argument("Error in tryDown");
+        }
+    }//end of try
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return nullptr;
+    }//end of catch
+}//end of down
+
+
+Node* Node::Left()
+{
+    try
+    {
+        if(zero_col != 0 && lastMove != RIGHT)
+        {
+            leftMove = new Node(this);
+            leftMove->board.at(zero_row).at(zero_col) = leftMove->board.at(zero_row).at(zero_col-1);
+            leftMove->board.at(zero_row).at(zero_col-1) = 0; 
+
+            leftMove->zero_col--;
+
+            leftMove->parent = this;
+            leftMove->lastMove = LEFT;
+
+
+            return leftMove;
+        }
+        else
+        {
+            throw invalid_argument("Error in tryLeft");
+        }
+    }//end of try
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return nullptr;
+    }//end of catch
+}//end of left
+
+Node* Node::Right()
+{
+    try
+    {
+        if(zero_col !=  board.at(0).size()-1 && lastMove != LEFT)
+        {
+            rightMove = new Node(this);
+            rightMove->board.at(zero_row).at(zero_col) = rightMove->board.at(zero_row).at(zero_col+1);
+            rightMove->board.at(zero_row).at(zero_col+1) = 0; 
+
+            rightMove->zero_col++;
+
+            rightMove->parent = this;
+            rightMove->lastMove = RIGHT;
+            return rightMove;
+        }
+        else
+        {
+            throw invalid_argument("Error in tryRight");
+        }
+    }//end of try
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return nullptr;
+    }//end of catch
+}//end of Right
+
+bool Node::in(vector<Node*> input)
+{
+    for(Node* n : input)
+    {
+        if( this == n)
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+
+bool Node::in(queue<Node*> input)
+{
+    while(!input.empty())
+    {
+        if( this == input.front())
+        {
+            return true;
+            break;
+        }
+        input.pop();
+    }
+    return false;
+}
+
+validMoves Node::getLastMove()
+{
+    return this->lastMove;
+}
+
+void Node::printLastMove()
+{
+    if(lastMove == UP)
+        cout << "Moved UP" << endl;
+    else if(lastMove == DOWN)
+        cout << "Moved DOWN" << endl;
+    else if(lastMove == LEFT)
+        cout << "Moved LEFT" << endl;
+    else if(lastMove == RIGHT)
+        cout << "Moved RIGHT" << endl;
+    else
+        cout << "Starting State" <<endl;
+    for(int i = 0; i < board.size(); i++)
+    {
+        for(int j = 0; j < board.at(i).size(); j++)
+        {
+            cout<<board.at(i).at(j)<<"\t";
+        }//end of inner loop
+        cout<<endl;
+    }//end of outer loop
+    cout<<endl;
+}//end of print last move
--- src/node.h
+++ src/node.h
@@ -2,12 +2,13 @@
 #define __NODE_H
 
 #include <vector>
-
+#include <queue>
 #include <iostream>
 
 
 using namespace std;
 
+enum validMoves {UP, DOWN, RIGHT, LEFT, START};
 class Node
 {
     private:
@@ -15,6 +16,9 @@ class Node
         vector<vector<int>> board;
         int zero_row = -1;
         int zero_col = -1;
+
+        validMoves lastMove;
+
         Node* rightMove;
         Node* leftMove;
         Node* upMove;
@@ -22,23 +26,68 @@ class Node
     public:
         Node();
         ~Node();
-        Node(vector<vector<int>>, Node*);
+        Node(vector<vector<int>>, Node*, validMoves);
         Node(vector<vector<int>>);
-        Node(Node &);//copy constuctor
+        Node(Node *);//copy constuctor
         
-        inline bool operator==(const Node* rhs) 
-        {
-            if(this->board == rhs->board)
-                return true;
-            return false;
-        }
+        // bool operator==(const Node rhs) 
+        // {
+        //     cout<<"test test"<<endl;
+        //     if(board == rhs.board)
+        //         return true;
+        //     return false;
+        // }
 
         //operators
         bool tryUp();
         bool tryDown();
         bool tryRight();
         bool tryLeft();
+
+        bool in(vector<Node*>);
+        bool in(queue<Node*>);
         void printNode();
+
+        Node* Up();
+        Node* Down();
+        Node* Right();
+        Node* Left();
+
+        Node* getParent() {return this->parent;}
+
+        validMoves getLastMove();
+        vector<vector<int>> getBoard() {return board;};
+
+        
+        void printLastMove();
+        friend ostream& operator<<(ostream& out, const Node* n)
+        {
+            // cout<<"test teste "<<endl;
+        if(n->lastMove == UP)
+            out << "Moved UP" << endl;
+        else if(n->lastMove == DOWN)
+            out << "Moved DOWN" << endl;
+        else if(n->lastMove == LEFT)
+            out << "Moved LEFT" << endl;
+        else if(n->lastMove == RIGHT)
+            out << "Moved RIGHT" << endl;
+        else
+            out << "Starting State" <<endl;
+        for(int i = 0; i < n->board.size(); i++)
+        {
+            for(int j = 0; j < n->board.at(i).size(); j++)
+            {
+                out<<n->board.at(i).at(j)<<"\t";
+            }//end of inner loop
+            out<<endl;
+        }//end of outer loop
+        out<<endl;
+        return out;
+    }//end pf << overload
+
+        
+
 };//end of node class
 
+        
 #endif
